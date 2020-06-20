dd9096ff534186a1209e86c546737d12c620b538
DrakeWagner <65195847+DrakeWagner@users.noreply.github.com>
2020-06-20 16:21:50 -0400

    Add files via upload

--- char_word_count.sh
+++ char_word_count.sh
@@ -8,10 +8,18 @@ echo $file
 # If the file is a text file, returns the character and word counts
 if [ ${file: -4} == ".txt" ]
 then
+  echo "Text file entered..."
   echo -n "Number of characters: " 
   wc -m < $file
   echo -n "Number of words: "
+  wc -w < $filee
+elif [ ${file: -4} == ".csv" ]
+then
+  echo "CSV file entered..."
+  echo -n "Number of characters: "
+  wc -m < $file
+  echo -n "Number of words: "
   wc -w < $file
-else
-  echo That is NOT a text file
+  echo -n "Number of comma separated values: "
+  grep -o "," $file | wc -l
 fi
