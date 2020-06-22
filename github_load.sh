789d22de0d9bd3cde379f5e8eef2f99178541c77
DrakeWagner <65195847+DrakeWagner@users.noreply.github.com>
2020-06-22 19:12:11 -0400

    Add files via upload
    checks for chrome

--- github_load.sh
+++ github_load.sh
@@ -2,7 +2,17 @@
 
 # opens my github on google chrome (or chromium on the raspberrypi ;) )
 
-if ! [ -x "$(command -v chromium-browser)" ]; then
-  echo "chromium is not installed." >&2
-else echo "chromium is installed.""
+
+if [ ! -x "$(command -v chromium-browser)" ];
+then
+  echo "chromium is NOT installed. trying chrome..." >&2
+  google-chrome https://github.com/DrakeWagner
+  if [ ! -x "$(command -v chrome-browser)" ];
+  then
+    echo "success!"
+  else
+    echo "failed to run chrome."
+else
+  echo "chromium is installed."
+  chromium-browser https://github.com/DrakeWagner
 fi
