fad43d12cb26f4083948e249ca8111a2bcb8cea1
DrakeWagner <65195847+DrakeWagner@users.noreply.github.com>
2020-06-22 19:16:54 -0400

    Add files via upload
    Final

--- github_load.sh
+++ github_load.sh
@@ -7,12 +7,15 @@ if [ ! -x "$(command -v chromium-browser)" ];
 then
   echo "chromium is NOT installed. trying chrome..." >&2
   google-chrome https://github.com/DrakeWagner
+
   if [ ! -x "$(command -v chrome-browser)" ];
   then
     echo "success!"
   else
     echo "failed to run chrome."
+  fi
+
 else
-  echo "chromium is installed."
+  echo "chromium is installed. Running through chromium..."
   chromium-browser https://github.com/DrakeWagner
 fi
