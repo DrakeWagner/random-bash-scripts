f1d06edece32701a7de2e186c444bf320866087e
DrakeWagner <dbw2tn@virginia.edu>
2020-08-23 19:31:09 -0400

    finish variable practice section

--- bash_practice.sh
+++ bash_practice.sh
@@ -5,7 +5,30 @@ variable=myvariable
 echo "my variable is called: "${variable} #${} around variables
 age=22
 echo 'I am '${age}' years old'
+echo ''
+# More examples of variables within text:
+echo "I am $age years old"
+echo 'I am $age years old'
+echo 'I am '$age' years old'
+
+
 
 FILELIST=`ls`
 filewithtime=/tmp/my-dir/file_$(/bin/date +%Y-%m-%d).txt
 echo $filewithtime
+echo ${FILELIST}
+
+# using ` allows for a command to be used
+# using ' just puts the words into a string
+# command as a variable:
+print_working_directory=`pwd`
+print_working_directory_single_quotes='pwd'
+echo 'The current working directory is: '$print_working_directory
+echo $print_working_directory_single_quotes
+
+# double quotes preserve whitespace:
+greeting='Hello          world!'
+echo $greeting
+echo \'$greeting\'
+echo "$greeting"
+
