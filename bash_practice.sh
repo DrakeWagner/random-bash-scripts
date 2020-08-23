13b97ad4f1922fb03b99d2a864b8ea50e060c850
DrakeWagner <dbw2tn@virginia.edu>
2020-08-23 17:39:40 -0400

    add timestamp function

--- bash_practice.sh
+++ bash_practice.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-# ^Sha-bang tells where to find the interpreter
+# ^Sha-bang tells where to find the interpreter, and is not read as a comment
+
+variable=myvariable
+echo "my variable is called: "${variable} #${} around variables
+age=22
+echo 'I am '${age}' years old'
+
+FILELIST=`ls`
+filewithtime=/tmp/my-dir/file_$(/bin/date +%Y-%m-%d).txt
+echo $filewithtime
